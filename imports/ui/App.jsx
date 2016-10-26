fc0b43f069ef69d858932681855d8d59bcec534c
seasoul33 <hityang@gmail.com>
2016-10-26 10:06:36 +0800

    Sort topics sorting with a switch

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,32 +1,44 @@
 import React, { Component, PropTypes } from 'react';
-// import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
- 
+
 import { Topics } from '../api/topics.js';
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
  
 import Topic from './Topic.jsx';
 import Form from './Form.jsx';
+
+import Switch from 'react-toggle-switch';
  
 // App component - represents the whole app
 class App extends Component {
     constructor() {
         super();
-        this.state = {tags:[]};
+        this.state = {tags:[], sort_by_time:false};
+        this.style_enabled = {color:'black', opacity:1};
+        this.style_disabled = {color:'gray', opacity:0.5};
     }
     
     renderTopics() {
         if(this.props.currentUser) {
             let fileteredTopics;
+            let sortedTopics;
+
+            if(this.state.sort_by_time === true) {
+                sortedTopics = this.props.topics_time;
+            }
+            else {
+                sortedTopics = this.props.topics;
+            }
 
             if(this.state.tags.length !== 0) {
                 fileteredTopics = this.state.tags.reduce( function(resultTopics, tag) {
                                             return resultTopics.filter((topic) => (topic.tags.includes(tag)));
-                                        }, this.props.topics);
+                                        }, sortedTopics);
             }
             else {
-                fileteredTopics = [...this.props.topics];
+                fileteredTopics = [...sortedTopics];
             }
 
             // console.log(fileteredTopics);
@@ -56,12 +68,30 @@ class App extends Component {
         const tags = event.target.value.split(' ').filter((tag) => (tag !== ''));
         this.setState({tags});
     }
+
+    switchSorting() {
+        this.setState({sort_by_time:(!this.state.sort_by_time)});
+    }
+
+    toggleSwitchLabelStyle (labelnum){
+        if(labelnum === 1) {
+            return this.state.sort_by_time ? this.style_disabled : this.style_enabled;
+        }
+        else {
+            return this.state.sort_by_time ? this.style_enabled : this.style_disabled;
+        }
+    }
  
     render() {
         return (
             <div className="container">
                 <header>
-                    <h1>Wanted Board</h1>
+                    <h1 className="title">Wanted Board</h1><br/>
+                    <span>
+                        <span><label ref="switchlabel1" style={this.toggleSwitchLabelStyle(1)}>以附議數量排序 </label></span>
+                        <span><Switch on={this.state.sort_by_time} onClick={this.switchSorting.bind(this)} /></span> 
+                        <span><label ref="switchlabel2" style={this.toggleSwitchLabelStyle(2)}> 以提問時間排序</label></span>
+                    </span>
                     <AccountsUIWrapper />
                 </header>
 
@@ -93,12 +123,14 @@ class App extends Component {
 
 App.propTypes = {
     topics: PropTypes.array.isRequired,
+    topics_time: PropTypes.array.isRequired,
     currentUser: PropTypes.object,
 };
  
 export default createContainer(() => {
     return {
         topics: Topics.find({}, { sort: { seconded: -1 } }).fetch(),
+        topics_time: Topics.find({}, { sort: { raisedAt: -1 } }).fetch(),
         currentUser: Meteor.user(),
     };
 }, App);
